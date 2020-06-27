9f44872f278b71d19b271cbc82299877a38d6774
Sebastien Guillemot <sebastiengllmt@gmail.com>
2020-06-27 23:32:09 +0900

    add typescript example

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,123 @@
-// TODO: test stuff like fee calculation for a tx
+import CardanoWasm = require('rust-lib')
+import { expect } from 'chai'
+import 'mocha';
+import { mnemonicToEntropy } from 'bip39';
 
-// import { expect } from 'chai'
+function harden(num: number): number {
+  return 0x80000000 + num;
+}
 
-describe('Transaction', () => {
-  it('calculates transaction fee', () => {
+// Purpose derivation (See BIP43)
+enum Purpose {
+  CIP1852=1852, // see CIP 1852
+}
+
+// Cardano coin type (SLIP 44)
+enum CoinTypes {
+  CARDANO=1815,
+}
+
+enum ChainDerivation {
+  EXTERNAL=0, // from BIP44
+  INTERNAL=1, // from BIP44
+  CHIMERIC=2, // from CIP1852
+}
+
+function getCip1852Account(): CardanoWasm.Bip32PrivateKey {
+  const entropy = mnemonicToEntropy(
+    [ "test", "walk", "nut", "penalty", "hip", "pave", "soap", "entry", "language", "right", "filter", "choice" ].join(' ')
+  )
+  const rootKey = CardanoWasm.Bip32PrivateKey.from_bip39_entropy(
+    Buffer.from(entropy, 'hex'),
+    Buffer.from(''),
+  );
+  return rootKey
+    .derive(harden(Purpose.CIP1852))
+    .derive(harden(CoinTypes.CARDANO))
+    .derive(harden(0)); // account #0
+}
+
+describe('Addresses', () => {
+  it('derive base address', () => {
+    // from address test vectors
+
+    const cip1852Account = getCip1852Account();
+
+    const utxoPubKey = cip1852Account
+      .derive(ChainDerivation.EXTERNAL)
+      .derive(0)
+      .to_public();
+    const stakeKey = cip1852Account
+      .derive(ChainDerivation.CHIMERIC)
+      .derive(0)
+      .to_public();
+
+    const baseAddr = CardanoWasm.BaseAddress.new(
+      0,
+      CardanoWasm.StakeCredential.from_keyhash(utxoPubKey.hash()),
+      CardanoWasm.StakeCredential.from_keyhash(stakeKey.hash()),
+    );
+
+    // switch to proper test once Haskell Shelley implements proper address generation
+    expect(baseAddr.to_address().to_bech32()).to.eq('addr1qq8hc2lefnn0kuvur9jpnxnghnd9lm3jty0tsptjwvdsyrtpshk7al9e262uud767td297tp6qhqtlhg5q47q26fjcyw53g6a4mqsnj8nl6q9q');
+    // expect(baseAddr.to_address().to_bech32()).to.eq('addr1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwqcyl47r');
+  })
+});
+
+describe('Transactions', () => {
+  it('create transaction', () => {
+    const txInputs = CardanoWasm.TransactionInputs.new();
+    {
+      txInputs.add(
+        CardanoWasm.TransactionInput.new(
+          CardanoWasm.TransactionHash.from_bytes(
+            Buffer.from('3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7', 'hex'),
+          ),
+          0, // index
+        )
+      );
+    }
+    const txOutputs = CardanoWasm.TransactionOutputs.new();
+    {
+      txOutputs.add(
+        CardanoWasm.TransactionOutput.new(
+          CardanoWasm.Address.from_bytes(
+            Buffer.from('61a6274badf4c9ca583df893a73139625ff4dc73aaa3082e67d6d5d08e0ce3daa4', 'hex'),
+          ),
+          BigInt(1),
+        )
+      );
+    }
+    const txBody = CardanoWasm.TransactionBody.new(
+      txInputs,
+      txOutputs,
+      BigInt(42), // fee
+      10, // ttl
+    );
     
+    const witnesses = CardanoWasm.TransactionWitnessSet.new();
+    {
+      const vkeyWitnesses = CardanoWasm.Vkeywitnesses.new();
+
+      // const prvKey = CardanoWasm.PrivateKey.from_normal_bytes(
+      //   Buffer.from('f7955ca7a24889e892a74851712975c924d536d503eeb1c900a7431900633fb8', 'hex')
+      // );
+      // vkeyWitnesses.add(
+      //   CardanoWasm.Vkeywitness.new(
+      //     CardanoWasm.Vkey.from_bytes(
+      //       Buffer.from('e7d33eeb6f1df124f9f4c226428bc46b4c93ac4bc89dacc85748d1a2b47ded13', 'hex')
+      //     ),
+      //     prvKey.sign(
+      //       // TODO
+      //       Buffer.from('ff', 'hex')
+      //     ),
+      //   )
+      // );
+      witnesses.set_vkeys(vkeyWitnesses);
+    }
+    CardanoWasm.Transaction.new(
+      txBody,
+      witnesses,
+    );
   })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-const wasm = require('rust-lib')
+// This file is just to test stuff quickly. Look at the spec.ts files for working examples
 
-const fs = require('fs')
+// import wasm = require('rust-lib')
 
-let addr = wasm.Address.new_base(wasm.Keyhash.new([1, 3, 3, 7]), wasm.Keyhash.new([0, 1, 2, 3, 4]))
+// import fs = require('fs');
 
-console.log(`addr.to_bytes() = ${addr.to_bytes()}`)
+// const addr = wasm.Address.new_base(wasm.Keyhash.new([1, 3, 3, 7]), wasm.Keyhash.new([0, 1, 2, 3, 4]))
 
-fs.writeFile('addr_out.bin', new Buffer(addr.to_bytes()), (err) => {
-    if (err) throw err
-    console.log('saved')
-})
+// console.log(`addr.to_bytes() = ${addr.to_bytes()}`)
 
-let inputs = wasm.TransactionInputs.new()
-inputs.add(wasm.TransactionInput.new(wasm.Hash.new([5, 4, 6, 4, 5])), 0)
-let outputs = wasm.TransactionOutputs.new()
-outputs.add(wasm.TransactionOutput.new(addr, 666))
-let tx_body = wasm.TransactionBody.new(inputs, outputs)
+// fs.writeFile('addr_out.bin', new Buffer(addr.to_bytes()), (err) => {
+//     if (err) throw err
+//     console.log('saved')
+// })
 
-fs.writeFile('tx_body.bin', new Buffer(tx_body.to_bytes()), (err) => {
-    if (err) throw err
-    console.log('saved')
-})
+// const inputs = wasm.TransactionInputs.new()
+// inputs.add(wasm.TransactionInput.new(wasm.Hash.new([5, 4, 6, 4, 5])), 0)
+// const outputs = wasm.TransactionOutputs.new()
+// outputs.add(wasm.TransactionOutput.new(addr, 666))
+// const tx_body = wasm.TransactionBody.new(inputs, outputs)
+
+// fs.writeFile('tx_body.bin', new Buffer(tx_body.to_bytes()), (err) => {
+//     if (err) throw err
+//     console.log('saved')
+// })
