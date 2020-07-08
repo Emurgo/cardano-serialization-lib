eff54ffd177ed71402845cebce0b5a6977110ab6
rooooooooob <raftias@gmail.com>
2020-07-08 00:59:53 -0500

    Make Coin its own class
    Instead of just a u64, which is converted to BigNum in wasm, we have our
    own wrapper type similar to js-chain-libs.
    
    The reason for this is that BigNUm might not exist in all frameworks
    (e.g. ReactNative) so we need to have the ability to create/get them as
    strings.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -38,7 +38,47 @@ impl UnitInterval {
     }
 }
 
-type Coin = u64;
+// Specifies an amount of ADA in terms of lovelace
+// String functions are for environemnts that don't support u64 or BigInt/etc
+#[wasm_bindgen]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Coin(u64);
+
+to_from_bytes!(Coin);
+
+#[wasm_bindgen]
+impl Coin {
+    // May not be supported in all environments as it maps to BigInt with wasm_bindgen
+    pub fn new(value: u64) -> Coin {
+        Self(value)
+    }
+
+    // Create a Coin from a standard rust string representation
+    pub fn from_str(string: &str) -> Result<Coin, JsValue> {
+        string.parse::<u64>()
+            .map_err(|e| JsValue::from_str(&format! {"{:?}", e}))
+            .map(Coin)
+    }
+
+    // String representation of the Coin value for use from environemtnst hat don't support BigInt
+    pub fn to_str(&self) -> String {
+        format!("{}", self.0)
+    }
+
+    pub fn checked_add(&self, other: &Coin) -> Result<Coin, JsValue> {
+        match self.0.checked_add(other.0) {
+            Some(value) => Ok(Coin(value)),
+            None => Err(JsValue::from_str("overflow")),
+        }
+    }
+
+    pub fn checked_sub(&self, other: &Coin) -> Result<Coin, JsValue> {
+        match self.0.checked_sub(other.0) {
+            Some(value) => Ok(Coin(value)),
+            None => Err(JsValue::from_str("underflow")),
+        }
+    }
+}
 
 type Epoch = u32;
 
--- rust/src/serialization.rs
+++ rust/src/serialization.rs
@@ -52,6 +52,21 @@ impl DeserializeEmbeddedGroup for UnitInterval {
     }
 }
 
+impl cbor_event::se::Serialize for Coin {
+    fn serialize<'se, W: Write>(&self, serializer: &'se mut Serializer<W>) -> cbor_event::Result<&'se mut Serializer<W>> {
+        serializer.write_unsigned_integer(self.0)
+    }
+}
+
+impl Deserialize for Coin {
+    fn deserialize<R: BufRead + Seek>(raw: &mut Deserializer<R>) -> Result<Self, DeserializeError> {
+        match raw.unsigned_integer() {
+            Ok(value) => Ok(Self(value)),
+            Err(e) => Err(DeserializeError::new("Coin", DeserializeFailure::CBOR(e))),
+        }
+    }
+}
+
 impl cbor_event::se::Serialize for Transaction {
     fn serialize<'se, W: Write>(&self, serializer: &'se mut Serializer<W>) -> cbor_event::Result<&'se mut Serializer<W>> {
         serializer.write_array(cbor_event::Len::Len(3))?;
