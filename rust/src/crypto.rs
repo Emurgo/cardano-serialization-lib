ccc19345ef8c1a8b486bd3793682887afaf637ab
rooooooooob <raftias@gmail.com>
2020-08-10 22:52:00 -0500

    Added in Update type
    Update taken from shelley.cddl with Nonce hand-coded based on the
    shelley code (and conforms to the definition in  mock/crypto.cddl)

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -728,3 +728,115 @@ impl_hash_type!(GenesisDelegateHash, 28);
 impl_hash_type!(GenesisHash, 28);
 impl_hash_type!(MetadataHash, 32);
 impl_hash_type!(VRFKeyHash, 28);
+
+// Evolving nonce type (used for Update's crypto)
+#[wasm_bindgen]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Nonce {
+    hash: Option<[u8; 32]>,
+}
+
+to_from_bytes!(Nonce);
+
+// can't export consts via wasm_bindgen
+impl Nonce {
+    pub const HASH_LEN: usize = 32;
+}
+
+#[wasm_bindgen]
+impl Nonce {
+    pub fn new_identity() -> Nonce {
+        Self {
+            hash: None,
+        }
+    }
+
+    pub fn new_from_hash(hash: Vec<u8>) -> Result<Nonce, JsValue> {
+        use std::convert::TryInto;
+        match hash[..Self::HASH_LEN].try_into() {
+            Ok(bytes_correct_size) => Ok(Self {
+                hash: Some(bytes_correct_size),
+            }),
+            Err(e) => Err(JsValue::from_str(&e.to_string())),
+        }
+    }
+
+    pub fn get_hash(&self) -> Option<Vec<u8>> {
+        Some(self.hash?.to_vec())
+
+    }
+}
+
+impl cbor_event::se::Serialize for Nonce {
+    fn serialize<'se, W: std::io::Write>(&self, serializer: &'se mut Serializer<W>) -> cbor_event::Result<&'se mut Serializer<W>> {
+        match &self.hash {
+            Some(hash) => {
+                serializer.write_array(cbor_event::Len::Len(2))?;
+                serializer.write_unsigned_integer(1)?;
+                serializer.write_bytes(hash)
+            },
+            None => {
+                serializer.write_array(cbor_event::Len::Len(1))?;
+                serializer.write_unsigned_integer(0)
+            },
+        }
+    }
+}
+
+impl Deserialize for Nonce {
+    fn deserialize<R: std::io::BufRead>(raw: &mut Deserializer<R>) -> Result<Self, DeserializeError> {
+        use std::convert::TryInto;
+        (|| -> Result<Self, DeserializeError> {
+            let len = raw.array()?;
+            let hash = match raw.unsigned_integer()? {
+                0 => None,
+                1 => {
+                    let bytes = raw.bytes()?;
+                    if bytes.len() != Self::HASH_LEN {
+                        return Err(DeserializeFailure::CBOR(cbor_event::Error::WrongLen(Self::HASH_LEN as u64, cbor_event::Len::Len(bytes.len() as u64), "hash length")).into());
+                    }
+                    Some(bytes[..Self::HASH_LEN].try_into().unwrap())
+                },
+                _ => return Err(DeserializeFailure::NoVariantMatched.into()),
+            };
+            match len {
+                cbor_event::Len::Len(n) => {
+                    let correct_len = match n {
+                        1 => hash.is_none(),
+                        2 => hash.is_some(),
+                        _ => false,
+                    };
+                    if !correct_len {
+                        return Err(DeserializeFailure::NoVariantMatched.into());
+                    }
+                },
+                cbor_event::Len::Indefinite => match raw.special()? {
+                    CBORSpecial::Break => /* it's ok */(),
+                    _ => return Err(DeserializeFailure::EndingBreakMissing.into()),
+                },
+            };
+            Ok(Self {
+                hash,
+            })
+        })().map_err(|e| e.annotate(stringify!($name)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn nonce_identity() {
+        let orig = Nonce::new_identity();
+        let deser = Nonce::deserialize(&mut Deserializer::from(std::io::Cursor::new(orig.to_bytes()))).unwrap();
+        assert_eq!(orig.to_bytes(), deser.to_bytes());
+    }
+
+    #[test]
+    fn nonce_hash() {
+        let orig = Nonce::new_from_hash(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]).unwrap();
+        let deser = Nonce::deserialize(&mut Deserializer::from(std::io::Cursor::new(orig.to_bytes()))).unwrap();
+        assert_eq!(orig.to_bytes(), deser.to_bytes());
+    }
+}
