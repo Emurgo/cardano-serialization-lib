9e2fd0c0b0871a82a621c0e043469f819e4ec2a4
José Ortiz <kotshie@gmail.com>
2020-08-20 17:52:18 -0700

    removes KES code

--- rust/src/chain_crypto/algorithms/mod.rs
+++ rust/src/chain_crypto/algorithms/mod.rs
@@ -2,12 +2,8 @@ pub mod ed25519;
 pub mod ed25519_derive;
 pub mod ed25519_extended;
 pub mod legacy_daedalus;
-pub mod sumed25519;
 
 pub use ed25519::Ed25519;
-//pub(crate) use ed25519::Pub;
 pub use ed25519_derive::Ed25519Bip32;
 pub use ed25519_extended::Ed25519Extended;
-//pub(crate) use ed25519_extended::ExtendedPriv;
 pub use legacy_daedalus::LegacyDaedalus;
-pub use sumed25519::SumEd25519_12;
--- rust/src/chain_crypto/mod.rs
+++ rust/src/chain_crypto/mod.rs
@@ -1,14 +1,10 @@
 pub mod algorithms;
-pub mod asymlock;
 pub mod bech32;
 pub mod derive;
 pub mod digest;
-mod evolving;
 pub mod hash;
-mod kes;
 mod key;
 mod sign;
-mod vrf;
 
 cfg_if! {
     if #[cfg(test)] {
@@ -17,10 +13,9 @@ cfg_if! {
         pub mod testing;
     }
 }
-pub mod role;
 
-pub use evolving::{EvolvingStatus, KeyEvolvingAlgorithm};
-pub use kes::KeyEvolvingSignatureAlgorithm;
+pub use algorithms::*;
+pub use hash::{Blake2b256, Sha3_256};
 pub use key::{
     AsymmetricKey, AsymmetricPublicKey, KeyPair, PublicKey, PublicKeyError, PublicKeyFromStrError,
     SecretKey, SecretKeyError, SecretKeySizeStatic,
@@ -29,10 +24,3 @@ pub use sign::{
     Signature, SignatureError, SignatureFromStrError, SigningAlgorithm, Verification,
     VerificationAlgorithm,
 };
-pub use vrf::{
-    vrf_evaluate_and_prove, vrf_verified_get_output, vrf_verify, VRFVerification,
-    VerifiableRandomFunction,
-};
-
-pub use algorithms::*;
-pub use hash::{Blake2b256, Sha3_256};
