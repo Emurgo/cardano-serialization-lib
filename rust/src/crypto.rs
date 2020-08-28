119cc0c46e0ff907d9387bc85385c26feb087c5c
Sebastien Guillemot <sebastiengllmt@gmail.com>
2020-08-28 10:09:12 +0900

    Add 128-byte xprv support

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -55,6 +55,34 @@ impl Bip32PrivateKey {
         Bip32PrivateKey(crypto::derive::derive_sk_ed25519(&self.0, index))
     }
 
+    /// 128-byte xprv a key format in Cardano that some software still uses or requires
+    /// the traditional 96-byte xprv is simply encoded as
+    /// prv | chaincode
+    /// however, because some software may not know how to compute a public key from a private key,
+    /// the 128-byte inlines the public key in the following format
+    /// prv | pub | chaincode
+    /// so be careful if you see the term "xprv" as it could refer to either one
+    /// our library does not require the pub (instead we compute the pub key when needed)
+    pub fn from_128_xprv(bytes: &[u8]) -> Result<Bip32PrivateKey, JsValue> {
+        let mut buf = [0; 96];
+        buf[0..64].clone_from_slice(&bytes[0..64]);
+        buf[64..96].clone_from_slice(&bytes[96..128]);
+
+        Bip32PrivateKey::from_bytes(&buf)
+    }
+    /// see from_128_xprv
+    pub fn to_128_xprv(&self) -> Vec<u8> {
+        let prv_key = self.to_raw_key().as_bytes();
+        let pub_key = self.to_public().to_raw_key().as_bytes();
+        let cc = self.chaincode();
+
+        let mut buf = [0; 128];
+        buf[0..64].clone_from_slice(&prv_key);
+        buf[64..96].clone_from_slice(&pub_key);
+        buf[96..128].clone_from_slice(&cc);
+        buf.to_vec()
+    }
+
     pub fn generate_ed25519_bip32() -> Result<Bip32PrivateKey, JsValue> {
         OsRng::new()
             .map(crypto::SecretKey::<crypto::Ed25519Bip32>::generate)
@@ -915,5 +943,20 @@ mod tests {
         let orig = Nonce::new_from_hash(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]).unwrap();
         let deser = Nonce::deserialize(&mut Deserializer::from(std::io::Cursor::new(orig.to_bytes()))).unwrap();
         assert_eq!(orig.to_bytes(), deser.to_bytes());
+    }
+
+    #[test]
+    fn xprv_128_test() {
+        // art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy
+        let entropy = [0x0c, 0xcb, 0x74, 0xf3, 0x6b, 0x7d, 0xa1, 0x64, 0x9a, 0x81, 0x44, 0x67, 0x55, 0x22, 0xd4, 0xd8, 0x09, 0x7c, 0x64, 0x12];
+        let root_key = Bip32PrivateKey::from_bip39_entropy(&entropy, &[]);
+
+        let xprv_128 = root_key.to_128_xprv();
+        // test the 128 xprv is the right format
+        assert_eq!(hex::encode(&xprv_128), "b8f2bece9bdfe2b0282f5bad705562ac996efb6af96b648f4445ec44f47ad95c10e3d72f26ed075422a36ed8585c745a0e1150bcceba2357d058636991f38a37cf76399a210de8720e9fa894e45e41e29ab525e30bc402801c076250d1585bcd91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4");
+        let root_key_copy = Bip32PrivateKey::from_128_xprv(&xprv_128).unwrap();
+
+        // test converting to and back is equivalent to the identity function
+        assert_eq!(root_key.to_bech32(), root_key_copy.to_bech32());
     }
 }
