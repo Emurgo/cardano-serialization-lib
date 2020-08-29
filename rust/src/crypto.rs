d3b00e53e2d800594b2936b73ea22b70c1121f78
Sebastien Guillemot <sebastiengllmt@gmail.com>
2020-08-29 11:22:19 +0900

    bug fix: chaincode getter on Bip32PublicKey

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -191,9 +191,9 @@ impl Bip32PublicKey {
     }
 
     pub fn chaincode(&self) -> Vec<u8> {
-        const ED25519_PRIVATE_KEY_LENGTH: usize = 64;
-        const XPRV_SIZE: usize = 96;
-        self.0.as_ref()[ED25519_PRIVATE_KEY_LENGTH..XPRV_SIZE].to_vec()
+        const ED25519_PUBLIC_KEY_LENGTH: usize = 32;
+        const XPUB_SIZE: usize = 64;
+        self.0.as_ref()[ED25519_PUBLIC_KEY_LENGTH..XPUB_SIZE].to_vec()
     }
 }
 
@@ -951,6 +951,7 @@ mod tests {
         let entropy = [0x0c, 0xcb, 0x74, 0xf3, 0x6b, 0x7d, 0xa1, 0x64, 0x9a, 0x81, 0x44, 0x67, 0x55, 0x22, 0xd4, 0xd8, 0x09, 0x7c, 0x64, 0x12];
         let root_key = Bip32PrivateKey::from_bip39_entropy(&entropy, &[]);
 
+        assert_eq!(hex::encode(&root_key.as_bytes()), "b8f2bece9bdfe2b0282f5bad705562ac996efb6af96b648f4445ec44f47ad95c10e3d72f26ed075422a36ed8585c745a0e1150bcceba2357d058636991f38a3791e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4");
         let xprv_128 = root_key.to_128_xprv();
         // test the 128 xprv is the right format
         assert_eq!(hex::encode(&xprv_128), "b8f2bece9bdfe2b0282f5bad705562ac996efb6af96b648f4445ec44f47ad95c10e3d72f26ed075422a36ed8585c745a0e1150bcceba2357d058636991f38a37cf76399a210de8720e9fa894e45e41e29ab525e30bc402801c076250d1585bcd91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4");
@@ -958,5 +959,18 @@ mod tests {
 
         // test converting to and back is equivalent to the identity function
         assert_eq!(root_key.to_bech32(), root_key_copy.to_bech32());
+    }
+
+    #[test]
+    fn chaincode_gen() {
+        // art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy
+        let entropy = [0x0c, 0xcb, 0x74, 0xf3, 0x6b, 0x7d, 0xa1, 0x64, 0x9a, 0x81, 0x44, 0x67, 0x55, 0x22, 0xd4, 0xd8, 0x09, 0x7c, 0x64, 0x12];
+        let root_key = Bip32PrivateKey::from_bip39_entropy(&entropy, &[]);
+
+        let prv_chaincode = root_key.chaincode();
+        assert_eq!(hex::encode(&prv_chaincode), "91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4");
+
+        let pub_chaincode = root_key.to_public().chaincode();
+        assert_eq!(hex::encode(&pub_chaincode), "91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4");
     }
 }
