5dfc32edfc03b63cbc9764c0b771e7ba6e2bba6a
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2022-02-15 17:00:30 +0300

    Reverted the previous changes and updated the witness-set field-4 serialization

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -908,7 +908,7 @@ pub fn hash_script_data(redeemers: &Redeemers, cost_models: &Costmdls, datums: O
         */
         buf.push(0x80);
         if let Some(d) = &datums {
-            buf.extend(d.clone_as_definite().to_bytes());
+            buf.extend(d.to_bytes());
         }
         buf.push(0xA0);
     } else {
@@ -921,7 +921,7 @@ pub fn hash_script_data(redeemers: &Redeemers, cost_models: &Costmdls, datums: O
         */
         buf.extend(redeemers.to_bytes());
         if let Some(d) = &datums {
-            buf.extend(d.clone_as_definite().to_bytes());
+            buf.extend(d.to_bytes());
         }
         buf.extend(cost_models.language_views_encoding());
     }
@@ -2173,7 +2173,7 @@ mod tests {
 
         assert_eq!(
             hex::encode(script_data_hash.to_bytes()),
-            "57240d358f8ab6128c4a66340271e4fec39b4971232add308f01a5809313adcf"
+            "4415e6667e6d6bbd992af5092d48e3c2ba9825200d0234d2470068f7f0f178b3"
         );
     }
 
