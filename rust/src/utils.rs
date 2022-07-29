004aa4f2732ca223b87f854330bc3093fac8d69f
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2022-07-29 22:18:16 +0300

    flowgen update

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -46,61 +46,6 @@ macro_rules! to_from_json {
     }
 }
 
-#[macro_export]
-macro_rules! from_hex {
-    // Custom from_bytes() code
-    ($name:ident, $data: ident, $body:block) => {
-        // wasm-exposed JsError return - JsError panics when used outside wasm
-        #[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
-        #[wasm_bindgen]
-        impl $name {
-            pub fn from_hex($data: &str) -> Result<$name, JsError> {
-                match hex::decode($data) {
-                    Ok(_) => Ok($body?),
-                    Err(e) => Err(JsError::from_str(&e.to_string()))
-                }
-
-            }
-        }
-        // non-wasm exposed DeserializeError return
-        #[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"))))]
-        impl $name {
-            pub fn from_hex($data: &str) -> Result<$name, DeserializeError> $body
-        }
-    };
-    // Uses Deserialize trait to auto-generate one
-    ($name:ident) => {
-        from_hex!($name, hex_str, {
-            let mut raw = Deserializer::from(std::io::Cursor::new(hex::decode(hex_str).unwrap()));
-            Self::deserialize(&mut raw)
-        });
-    };
-}
-
-#[macro_export]
-macro_rules! to_hex {
-    ($name:ident) => {
-        #[wasm_bindgen]
-        impl $name {
-            pub fn to_hex(&self) -> String {
-                let mut buf = Serializer::new_vec();
-                self.serialize(&mut buf).unwrap();
-                hex::encode(buf.finalize())
-            }
-        }
-    }
-}
-
-#[macro_export]
-macro_rules! to_from_bytes {
-    ($name:ident) => {
-        to_bytes!($name);
-        from_bytes!($name);
-        to_hex!($name);
-        from_hex!($name);
-    }
-}
-
 #[wasm_bindgen]
 #[derive(Clone, Debug)]
 pub struct TransactionUnspentOutput {
