5fe060999a5388cba7d6f5df5193ea4e2d3c0f61
Denis <nebytov.d@gmail.com>
2023-02-08 01:06:53 +0300

    prettified angular asmjs

--- templates/angular-asmjs/src/app/app.module.ts
+++ templates/angular-asmjs/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { CslExampleComponent } from './csl-example/csl-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CslExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
