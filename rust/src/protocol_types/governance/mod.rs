0f1a1a4fc322f773a0b905d5d48768d368be0b91
lisicky <lisicky@inbox.ru>
2023-08-12 02:41:58 +0400

    update imports

--- rust/src/protocol_types/governance/mod.rs
+++ rust/src/protocol_types/governance/mod.rs
@@ -6,3 +6,18 @@ pub use anchor::*;
 
 mod voter;
 pub use voter::*;
+
+mod voters;
+pub use voters::*;
+
+mod voting_procedure;
+pub use voting_procedure::*;
+
+mod voting_procedures;
+pub use voting_procedures::*;
+
+mod governance_action_id;
+pub use governance_action_id::*;
+
+mod governance_action_ids;
+pub use governance_action_ids::*;
--- rust/src/serialization/governance/mod.rs
+++ rust/src/serialization/governance/mod.rs
@@ -6,3 +6,12 @@ pub use anchor::*;
 
 mod voter;
 pub use voter::*;
+
+mod voting_procedure;
+pub use voting_procedure::*;
+
+mod voting_procedures;
+pub use voting_procedures::*;
+
+mod governance_action_id;
+pub use governance_action_id::*;
