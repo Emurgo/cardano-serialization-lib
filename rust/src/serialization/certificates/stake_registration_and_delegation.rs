390c257d0f005e1ced8d05a789c56d7a6121f8be
lisicky <lisicky@inbox.ru>
2023-08-12 00:31:34 +0400

    add stake_reg_deleg_cert

---
+++ rust/src/serialization/certificates/stake_registration_and_delegation.rs
@@ -0,0 +1,66 @@
+use crate::*;
+
+const STAKE_REG_DELEG_CERT_INDEX: u64 = 11;
+
+impl cbor_event::se::Serialize for StakeRegistrationAndDelegation {
+    fn serialize<'se, W: Write>(
+        &self,
+        serializer: &'se mut Serializer<W>,
+    ) -> cbor_event::Result<&'se mut Serializer<W>> {
+        serializer.write_array(cbor_event::Len::Len(4))?;
+        serializer.write_unsigned_integer(STAKE_REG_DELEG_CERT_INDEX)?;
+        self.stake_credential.serialize(serializer)?;
+        self.pool_keyhash.serialize(serializer)?;
+        self.coin.serialize(serializer)?;
+        Ok(serializer)
+    }
+}
+
+impl Deserialize for StakeRegistrationAndDelegation {
+    fn deserialize<R: BufRead + Seek>(raw: &mut Deserializer<R>) -> Result<Self, DeserializeError> {
+        (|| -> Result<_, DeserializeError> {
+            let len = raw.array()?;
+
+            if let cbor_event::Len::Len(n) = len {
+                if n != 4 {
+                    return Err(DeserializeFailure::CBOR(cbor_event::Error::WrongLen(
+                        4,
+                        len,
+                        "(cert_index, stake_credential, pool_keyhash, coin)",
+                    ))
+                        .into());
+                }
+            }
+
+            let cert_index = raw.unsigned_integer()?;
+            if cert_index != STAKE_REG_DELEG_CERT_INDEX {
+                return Err(DeserializeFailure::FixedValueMismatch {
+                    found: Key::Uint(cert_index),
+                    expected: Key::Uint(STAKE_REG_DELEG_CERT_INDEX),
+                })
+                    .map_err(|e| DeserializeError::from(e).annotate("cert_index"));
+            }
+
+            let stake_credential =
+                StakeCredential::deserialize(raw).map_err(|e| e.annotate("stake_credential"))?;
+
+            let pool_keyhash =
+                Ed25519KeyHash::deserialize(raw).map_err(|e| e.annotate("pool_keyhash"))?;
+
+            let coin = Coin::deserialize(raw).map_err(|e| e.annotate("coin"))?;
+
+            if let cbor_event::Len::Indefinite = len {
+                if raw.special()? != CBORSpecial::Break {
+                    return Err(DeserializeFailure::EndingBreakMissing.into());
+                }
+            }
+
+            return Ok(StakeRegistrationAndDelegation {
+                stake_credential,
+                pool_keyhash,
+                coin,
+            });
+        })()
+            .map_err(|e| e.annotate("StakeRegistrationAndDelegation"))
+    }
+}
