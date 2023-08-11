bb8e6e5847579781f4f7336e2a5029649c7d3324
lisicky <lisicky@inbox.ru>
2023-08-12 00:29:46 +0400

    add anchor

---
+++ rust/src/protocol_types/governance/anchor.rs
@@ -0,0 +1,37 @@
+use crate::*;
+
+#[wasm_bindgen]
+#[derive(
+    Clone,
+    Debug,
+    Hash,
+    Eq,
+    Ord,
+    PartialEq,
+    PartialOrd,
+    serde::Serialize,
+    serde::Deserialize,
+    JsonSchema,
+)]
+pub struct Anchor {
+    pub(crate) anchor_url: URL,
+    pub(crate) anchor_data_hash: AnchorDataHash,
+}
+
+#[wasm_bindgen]
+impl Anchor {
+    pub fn anchor_url(&self) -> URL {
+        self.anchor_url.clone()
+    }
+
+    pub fn anchor_data_hash(&self) -> AnchorDataHash {
+        self.anchor_data_hash.clone()
+    }
+
+    pub fn new(anchor_url: &URL, anchor_data_hash: &AnchorDataHash) -> Self {
+        Self {
+            anchor_url: anchor_url.clone(),
+            anchor_data_hash: anchor_data_hash.clone(),
+        }
+    }
+}
