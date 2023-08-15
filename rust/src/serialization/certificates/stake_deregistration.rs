53b5ab81f154f3944b7c6cc734ce2dd831a00831
lisicky <lisicky@inbox.ru>
2023-08-15 19:44:03 +0400

    update stake dereg serialisation

--- rust/src/serialization/certificates/stake_deregistration.rs
+++ rust/src/serialization/certificates/stake_deregistration.rs
@@ -1,26 +1,43 @@
+use cbor_event::Len;
 use crate::*;
 
-pub(super) const DEREG_STAKE_CERT_INDEX: u64 = 1;
+pub(super) const DEREG_STAKE_CERT_LEGACY_INDEX: u64 = 1;
+pub(super) const DEREG_STAKE_CERT_CONWAY_INDEX: u64 = 8;
 
 impl cbor_event::se::Serialize for StakeDeregistration {
     fn serialize<'se, W: Write>(
         &self,
         serializer: &'se mut Serializer<W>,
     ) -> cbor_event::Result<&'se mut Serializer<W>> {
-        serializer.write_array(cbor_event::Len::Len(2))?;
-        self.serialize_as_embedded_group(serializer)
+        if self.coin.is_some() {
+            serialize_as_conway(self, serializer)
+        } else {
+            serialize_as_legacy(self, serializer)
+        }
     }
 }
 
-impl SerializeEmbeddedGroup for StakeDeregistration {
-    fn serialize_as_embedded_group<'se, W: Write>(
-        &self,
-        serializer: &'se mut Serializer<W>,
-    ) -> cbor_event::Result<&'se mut Serializer<W>> {
-        serializer.write_unsigned_integer(DEREG_STAKE_CERT_INDEX)?;
-        self.stake_credential.serialize(serializer)?;
-        Ok(serializer)
+fn serialize_as_legacy<'se, W: Write>(
+    cert: &StakeDeregistration,
+    serializer: &'se mut Serializer<W>,
+) -> cbor_event::Result<&'se mut Serializer<W>> {
+    serializer.write_array(cbor_event::Len::Len(2))?;
+    serializer.write_unsigned_integer(DEREG_STAKE_CERT_LEGACY_INDEX)?;
+    cert.stake_credential.serialize(serializer)?;
+    Ok(serializer)
+}
+
+fn serialize_as_conway<'se, W: Write>(
+    cert: &StakeDeregistration,
+    serializer: &'se mut Serializer<W>,
+) -> cbor_event::Result<&'se mut Serializer<W>> {
+    serializer.write_array(cbor_event::Len::Len(3))?;
+    serializer.write_unsigned_integer(DEREG_STAKE_CERT_CONWAY_INDEX)?;
+    cert.stake_credential.serialize(serializer)?;
+    if let Some(coin) = cert.coin {
+        coin.serialize(serializer)?;
     }
+    Ok(serializer)
 }
 
 impl Deserialize for StakeDeregistration {
@@ -29,19 +46,11 @@ impl Deserialize for StakeDeregistration {
             let len = raw.array()?;
             let ret = Self::deserialize_as_embedded_group(raw, len);
             match len {
-                cbor_event::Len::Len(_) =>
-                /* TODO: check finite len somewhere */
-                {
-                    ()
-                }
-                cbor_event::Len::Indefinite => match raw.special()? {
-                    CBORSpecial::Break =>
-                    /* it's ok */
-                    {
-                        ()
-                    }
+                Len::Indefinite => match raw.special()? {
+                    CBORSpecial::Break => {}
                     _ => return Err(DeserializeFailure::EndingBreakMissing.into()),
                 },
+                _ => {}
             }
             ret
         })()
@@ -52,26 +61,95 @@ impl Deserialize for StakeDeregistration {
 impl DeserializeEmbeddedGroup for StakeDeregistration {
     fn deserialize_as_embedded_group<R: BufRead + Seek>(
         raw: &mut Deserializer<R>,
-        _: cbor_event::Len,
+        len: Len,
     ) -> Result<Self, DeserializeError> {
-        (|| -> Result<_, DeserializeError> {
-            let cert_index = raw.unsigned_integer()?;
-            if cert_index != DEREG_STAKE_CERT_INDEX {
-                return Err(DeserializeFailure::FixedValueMismatch {
-                    found: Key::Uint(cert_index),
-                    expected: Key::Uint(DEREG_STAKE_CERT_INDEX),
-                }
-                .into());
-            }
-            Ok(())
-        })()
-        .map_err(|e| e.annotate("cert_index"))?;
-        let stake_credential =
-            (|| -> Result<_, DeserializeError> { Ok(StakeCredential::deserialize(raw)?) })()
-                .map_err(|e| e.annotate("stake_credential"))?;
-        Ok(StakeDeregistration {
-            stake_credential,
-            coin: None,
-        })
+        let cert_index = raw.unsigned_integer()?;
+        match cert_index {
+            DEREG_STAKE_CERT_LEGACY_INDEX => deserialize_legacy(raw, cert_index, len),
+            DEREG_STAKE_CERT_CONWAY_INDEX => deserialize_conway(raw, cert_index, len),
+            _ => Err(DeserializeFailure::FixedValuesMismatch {
+                found: Key::Uint(cert_index),
+                expected: vec![
+                    Key::Uint(DEREG_STAKE_CERT_LEGACY_INDEX),
+                    Key::Uint(DEREG_STAKE_CERT_CONWAY_INDEX),
+                ],
+            })
+                .map_err(|e| DeserializeError::from(e).annotate("cert_index")),
+        }
     }
 }
+
+fn deserialize_legacy<R: BufRead + Seek>(
+    raw: &mut Deserializer<R>,
+    cert_index: u64,
+    len: Len,
+) -> Result<StakeDeregistration, DeserializeError> {
+    (|| -> Result<_, DeserializeError> {
+        if let Len::Len(n) = len {
+            if n != 2 {
+                return Err(DeserializeFailure::CBOR(cbor_event::Error::WrongLen(
+                    2,
+                    len,
+                    "(cert_index, stake_credential)",
+                ))
+                    .into());
+            }
+        }
+
+        if cert_index != DEREG_STAKE_CERT_LEGACY_INDEX {
+            return Err(DeserializeFailure::FixedValueMismatch {
+                found: Key::Uint(cert_index),
+                expected: Key::Uint(DEREG_STAKE_CERT_LEGACY_INDEX),
+            })
+                .map_err(|e| DeserializeError::from(e).annotate("cert_index"));
+        }
+
+        let stake_credential =
+            StakeCredential::deserialize(raw).map_err(|e| e.annotate("stake_credential"))?;
+
+        return Ok(StakeDeregistration {
+            stake_credential,
+            coin: None,
+        });
+    })()
+        .map_err(|e| e.annotate("StakeDeregistration (legacy)"))
+}
+
+fn deserialize_conway<R: BufRead + Seek>(
+    raw: &mut Deserializer<R>,
+    cert_index: u64,
+    len: Len,
+) -> Result<StakeDeregistration, DeserializeError> {
+    (|| -> Result<_, DeserializeError> {
+        if let Len::Len(n) = len {
+            if n != 3 {
+                return Err(DeserializeFailure::CBOR(cbor_event::Error::WrongLen(
+                    3,
+                    len,
+                    "(cert_index, stake_credential, coin)",
+                ))
+                    .into());
+            }
+        }
+
+        if cert_index != DEREG_STAKE_CERT_CONWAY_INDEX {
+            return Err(DeserializeFailure::FixedValueMismatch {
+                found: Key::Uint(cert_index),
+                expected: Key::Uint(DEREG_STAKE_CERT_CONWAY_INDEX),
+            })
+                .map_err(|e| DeserializeError::from(e).annotate("cert_index"));
+        }
+
+        let stake_credential =
+            StakeCredential::deserialize(raw).map_err(|e| e.annotate("stake_credential"))?;
+
+        let coin = Coin::deserialize(raw).map_err(|e| e.annotate("coin"))?;
+
+        return Ok(StakeDeregistration {
+            stake_credential,
+            coin: Some(coin),
+        });
+    })()
+        .map_err(|e| e.annotate("StakeDeregistration (conway)"))
+}
+
