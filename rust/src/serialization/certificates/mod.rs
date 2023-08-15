074fee57c9264599e43f4f2214a64f2e7096059c
lisicky <lisicky@inbox.ru>
2023-08-15 19:44:38 +0400

    update mod file

--- rust/src/serialization/certificates/mod.rs
+++ rust/src/serialization/certificates/mod.rs
@@ -2,52 +2,21 @@ mod certificate;
 pub use certificate::*;
 
 mod genesis_key_delegation;
-pub use genesis_key_delegation::*;
 
 mod move_instantaneous_rewards_cert;
-pub use move_instantaneous_rewards_cert::*;
 
 mod pool_registration;
-pub use pool_registration::*;
-
 mod pool_retirement;
-pub use pool_retirement::*;
-
 mod stake_delegation;
-pub use stake_delegation::*;
-
 mod stake_deregistration;
-pub use stake_deregistration::*;
-
 mod stake_registration;
-pub use stake_registration::*;
-
 mod vote_delegation;
-pub use vote_delegation::*;
-
 mod stake_and_vote_delegation;
-pub use stake_and_vote_delegation::*;
-
 mod stake_registration_and_delegation;
-pub use stake_registration_and_delegation::*;
-
 mod stake_vote_registration_and_delegation;
-pub use stake_vote_registration_and_delegation::*;
-
 mod vote_registration_and_delegation;
-pub use vote_registration_and_delegation::*;
-
 mod committee_hot_key_registration;
-pub use committee_hot_key_registration::*;
-
 mod committee_hot_key_deregistration;
-pub use committee_hot_key_deregistration::*;
-
 mod drep_registration;
-pub use drep_registration::*;
-
 mod drep_deregistration;
-pub use drep_deregistration::*;
-
 mod drep_update;
-pub use drep_update::*;
