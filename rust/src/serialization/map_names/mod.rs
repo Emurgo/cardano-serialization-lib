61e3a115197cd2daa4b788f91740d4d3558c9ca1
lisicky <lisicky@inbox.ru>
2023-08-18 00:10:21 +0400

    move map names

---
+++ rust/src/serialization/map_names/mod.rs
@@ -0,0 +1,11 @@
+mod tx_body_names;
+pub(crate) use tx_body_names::*;
+
+mod witness_set_names;
+pub(crate) use witness_set_names::*;
+
+mod certificate_index_names;
+pub(crate) use certificate_index_names::*;
+
+mod voting_proposal_index_names;
+pub(crate) use voting_proposal_index_names::*;
