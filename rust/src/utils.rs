6c1e39726f6824d37729b470ad05f596073765db
lisicky <lisicky@inbox.ru>
2023-08-18 00:19:22 +0400

    remove moved code from utils

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1118,39 +1118,7 @@ impl From<BigNum> for BigInt
     }
 }
 
-// we use the cbor_event::Serialize trait directly
 
-// This is only for use for plain cddl groups who need to be embedded within outer groups.
-pub(crate) trait SerializeEmbeddedGroup {
-    fn serialize_as_embedded_group<'a, W: Write + Sized>(
-        &self,
-        serializer: &'a mut Serializer<W>,
-    ) -> cbor_event::Result<&'a mut Serializer<W>>;
-}
-
-// same as cbor_event::de::Deserialize but with our DeserializeError
-pub trait Deserialize {
-    fn deserialize<R: BufRead + Seek>(raw: &mut Deserializer<R>) -> Result<Self, DeserializeError>
-        where
-            Self: Sized;
-}
-
-// auto-implement for all cbor_event Deserialize implementors
-impl<T: cbor_event::de::Deserialize> Deserialize for T {
-    fn deserialize<R: BufRead + Seek>(raw: &mut Deserializer<R>) -> Result<T, DeserializeError> {
-        T::deserialize(raw).map_err(|e| DeserializeError::from(e))
-    }
-}
-
-// This is only for use for plain cddl groups who need to be embedded within outer groups.
-pub trait DeserializeEmbeddedGroup {
-    fn deserialize_as_embedded_group<R: BufRead + Seek>(
-        raw: &mut Deserializer<R>,
-        len: cbor_event::Len,
-    ) -> Result<Self, DeserializeError>
-        where
-            Self: Sized;
-}
 
 pub struct CBORReadLen {
     deser_len: cbor_event::Len,
