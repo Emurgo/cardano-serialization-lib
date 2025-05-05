1da6ca47da779d19a13db6909c89253fe8fe5ced
lisicky <lisicky@inbox.ru>
2025-05-05 20:51:14 +0200

    add internal function

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -840,6 +840,10 @@ impl Withdrawals {
                 .collect::<Vec<RewardAddress>>(),
         )
     }
+
+    pub(crate) fn as_vec(&self) -> Vec<(&RewardAddress, &Coin)> {
+        self.0.iter().collect()
+    }
 }
 
 impl serde::Serialize for Withdrawals {
