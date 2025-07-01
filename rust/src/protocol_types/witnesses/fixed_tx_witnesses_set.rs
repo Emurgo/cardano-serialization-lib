fbc940531beae85f6d7d734427ee234b0b510374
lisicky <lisicky@inbox.ru>
2025-07-02 00:52:42 +0200

    fix conditional

--- rust/src/protocol_types/witnesses/fixed_tx_witnesses_set.rs
+++ rust/src/protocol_types/witnesses/fixed_tx_witnesses_set.rs
@@ -84,7 +84,7 @@ impl FixedTxWitnessesSet {
         buf.finalize()
     }
 
-    #[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"), feature = "dont-expose-wasm")))]
+    #[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"), not(feature = "dont-expose-wasm"))))]
     pub fn from_bytes(data: Vec<u8>) -> Result<FixedTxWitnessesSet, DeserializeError> {
         let mut raw = Deserializer::from(std::io::Cursor::new(data));
         Self::deserialize(&mut raw)
